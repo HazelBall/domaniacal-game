ec8d622c1b361326acf0e6848c46e7cc70b15ac9
HazelBall <roundnround01@gmail.com>
2024-02-02 16:19:31 -0700

    Testing node.js

--- scripts/gameModel.js
+++ scripts/gameModel.js
@@ -1,7 +1,7 @@
 import Domino from "./Domino.js";
 import Stack from "./Stack.js";
 import Deck from "./Deck.js";
-import Player from "./Player.js"
+import Player from "./Player.js";
 
 const deck = new Deck(0, 8);
 const players = [
@@ -26,5 +26,23 @@ players.forEach((player) => {
     console.log("Stack:");
     console.log(player.stack.peek())
     console.log("Hand:");
-    player.hand.forEach((card) => {console.log("\t" + card.toString())});
+    let hand = "";
+    player.hand.forEach((card) => {hand += card.toString() + ", "});
+    console.log(hand);
 });
+
+// Game Model
+class GameModel {
+    constructor() {
+        this.gameStarted = false;
+        this.players = []; // array of all players
+    }
+
+    addPlayer = () => {
+
+    }
+
+    startGame = () => {
+        if(this.players.length < 2) throw "Player"
+    }
+}
---
+++ scripts/gameView.js
@@ -0,0 +1,20 @@
+class gameView {
+    // Create an element with an optional CSS class
+    createElement(tag, className) {
+        const element = document.createElement(tag)
+        if (className) element.classList.add(className)
+
+        return element
+    }
+
+    // Retrieve an element from the DOM
+    getElement(selector) {
+        const element = document.querySelector(selector)
+
+        return element
+    }
+
+    constructor() {
+        
+    }
+}
